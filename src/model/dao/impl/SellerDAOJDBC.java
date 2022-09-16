463e803ff3e408e86735af1930544b87bb80d1e0
Robert P Pereira <robertpaiva.df@gmail.com>
2022-09-16 01:08:16 -0300

    Criando findById Seller

--- src/model/dao/impl/SellerDAOJDBC.java
+++ src/model/dao/impl/SellerDAOJDBC.java
@@ -1,12 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DBException;
 import model.dao.SellerDAO;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDAOJDBC implements SellerDAO{
 
+	private Connection conn;
+	
+	public SellerDAOJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
@@ -27,8 +40,40 @@ public class SellerDAOJDBC implements SellerDAO{
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			
+			st = conn.prepareStatement(
+					  "SELECT seller.*,department.Name as DepName\r\n"
+					+ "FROM seller INNER JOIN department\r\n"
+					+ "ON seller.DepartmentId = department.Id\r\n"
+					+ "WHERE seller.Id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if(rs.next()) { //necessário para ir para o 1o registro do rs
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));
+				Seller obj = new Seller();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setDepartment(dep);
+				return obj;
+			}
+			return null;
+		} 
+		catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		}
+		finally{
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
 	}
 
 	@Override
